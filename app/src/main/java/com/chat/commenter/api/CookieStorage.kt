beb6cb8a8dbc3d99c9cdb8e0b30a453619b941b2
PP6464 <panthhp@outlook.com>
2025-02-18 14:33:05 +0000

    stop default value errors for cookies

--- app/src/main/java/com/chat/commenter/api/CookieStorage.kt
+++ app/src/main/java/com/chat/commenter/api/CookieStorage.kt
@@ -10,15 +10,17 @@ import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import androidx.core.content.edit
 
-class PersistentCookieStorage(context : Context) : CookiesStorage {
-	private val preferences: SharedPreferences = context.getSharedPreferences("cookies", Context.MODE_PRIVATE)
+class PersistentCookieStorage(context: Context) : CookiesStorage {
+	private val preferences: SharedPreferences =
+		context.getSharedPreferences("cookies", Context.MODE_PRIVATE)
 	private val mutex = Mutex()
 	private var cookies: MutableList<Cookie> = loadCookies()
 	
 	override suspend fun addCookie(requestUrl: Url, cookie: Cookie) {
 		cookies.removeIf { it.name == cookie.name && it.matches(requestUrl) }
-		cookies.add(cookie.copy(domain = requestUrl.host))
+		cookies.add(cookie.copy(domain = requestUrl.host, path = "/"))
 		println("ADDED A COOKIE: NAME=${cookie.name}, URL=$requestUrl, VALUE=${cookie.value}")
 		saveCookies()
 	}
@@ -29,18 +31,21 @@ class PersistentCookieStorage(context : Context) : CookiesStorage {
 	}
 	
 	override suspend fun get(requestUrl: Url): List<Cookie> = mutex.withLock {
-		cookies.filter { it.matches(requestUrl) }
+		cookies.filter { it.domain == requestUrl.host }
 	}
 	
 	private fun saveCookies() {
 		val json = Json.encodeToString(cookies)
-		preferences.edit().putString("cookies", json).apply()
+		preferences.edit { putString("cookies", json) }
 	}
 	
 	private fun loadCookies(): MutableList<Cookie> {
 		val json = preferences.getString("cookies", null) ?: "[]"
 		println("LOADED COOKIES FROM STORAGE")
-		println(Json.decodeFromString<MutableList<Cookie>>(json))
-		return Json.decodeFromString(json)
+		val cookies = Json.decodeFromString<List<Cookie>>(json).map {
+			if (it.path == null) it.copy(path = "/") else it
+		}.toMutableList()
+		println(cookies)
+		return cookies
 	}
 }
